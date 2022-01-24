bf225d9d225b9d87276933855741980ccc3e486a
GautamKJ <jha.gautam306@gmail.com>
2022-01-25 00:00:28 +0530

    fix the special Button issue

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ let screen = document.getElementById('screen');
 let screenValue = "";
 // let buttonText = "";
 let button = document.querySelectorAll('button');
+let special = document.querySelectorAll('special');
 for (item of button) {
     item.addEventListener('click', (e) => {
         // screenValue = item.innerText;
         buttonText = e.target.innerText;
         console.log(buttonText);
 
+        console.log("length ", screenValue.length + "\n");
 
         if (buttonText == 'C') {
             screenValue = "";
@@ -16,11 +18,11 @@ for (item of button) {
             console.log(screen.value)
         }
         else if (buttonText == '=') {
-            screenValue =eval(screenValue);              
-            screen.value =screenValue;
+            screenValue = eval(screenValue);
+            screen.value = screenValue;
             console.log(screen.value)
-            
-            
+
+
         }
         else if (buttonText == 'X') {
 
@@ -30,10 +32,24 @@ for (item of button) {
             screen.value = screenValue;
             console.log(screen.value)
         }
+        else if (screenValue!="" && (screenValue.charAt(screenValue.length - 1) == '+' || screenValue.charAt(screenValue.length - 1) == '-' || screenValue.charAt(screenValue.length - 1) == '*' || screenValue.charAt(screenValue.length - 1) == '/' || screenValue.charAt(screenValue.length - 1) == '%') && ((buttonText == '+') || (buttonText == '-') || (buttonText == '*') || (buttonText == '/') || (buttonText == '%'))) {
+            console.log("screenValue--> ", screenValue);
+            screenValue = screenValue.slice(0, - 1);
+            console.log("screenValue--> ", screenValue);
+            screen.value = screenValue;
+            screenValue += buttonText;
+            screen.value = screenValue;
+            console.log("screenValue--> ", screenValue);
+        }
+
         else {
             screenValue += buttonText;
             screen.value = screenValue;
             console.log(screen.value)
+
         }
+        
+
+
     });
 }
